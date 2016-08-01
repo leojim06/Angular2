6c84f857ebfbc5bd0033425bed1ff4f4997022ee
Javier Jiménez <leojim06gmail.com>
2016-07-31 21:23:18 -0500

    feat (hero): usa metodos creados post, put, delete
    Se modifica el código de heroes.component y hero-detail.component para que hagan
    uso de los metodos de post, put y delete que se crearon en el hero.service y se
    puedan agregar, modificar y eliminar heroes.  Además se modifican los archivos
    HTML respectivos para agregar los botones que harán estas funciones y usaran los
    metodos creados.  La aplicación ahora tiene el dashboard, la lista de heroes,
    agrega nuevos heroes, modifica heroes y los borra a traves de las interfaces
    diseñadas para el usuario

--- app/heroes.component.ts
+++ app/heroes.component.ts
@@ -3,17 +3,21 @@ import { Router } from '@angular/router';
 
 import { Hero } from './hero';
 import { HeroService } from './hero.service';
+import { HeroDetailComponent } from './hero-detail.component';
 
 
 @Component({
     selector: 'my-heroes',
     templateUrl: 'app/heroes.component.html',
-    styleUrls: ['app/heroes.component.css']
+    styleUrls: ['app/heroes.component.css'],
+    directives: [HeroDetailComponent]
 })
 
 export class HeroesComponent implements OnInit { 
     heroes: Hero[];
     selectedHero: Hero;
+    addingHero = false;
+    error: any;
 
     constructor(
         private router: Router,
@@ -32,5 +36,28 @@ export class HeroesComponent implements OnInit {
 
     gotoDetail() {
         this.router.navigate(['/detail', this.selectedHero.id]);
+    }
+
+    addHero() {
+        this.addingHero = true;
+        this.selectedHero = null;
+    }
+
+    close(savedHero: Hero) {
+        this.addingHero = false;
+        if (savedHero) { this.getHeroes(); }
+    }
+
+    deletedHero(hero: Hero, event: any) {
+        event.stopPropagation();
+        this.heroService
+            .delete(hero)
+            .then(res => {
+                this.heroes = this.heroes.filter(h => h !== hero);
+                if (this.selectedHero === hero) {
+                    this.selectedHero = null;
+                }
+            })
+            .catch(error => this.error = error);
     }
 }
