c8eecf74f74a7a31415d92489d28bcd3838574a2
Javier Jiménez <leojim06gmail.com>
2016-07-31 20:24:21 -0500

    refactor (hero.service): modifica servicio para usar http
    Se modifica el servicio hero.service para que pueda hacer operaciones con los
    verbos http y se agregan metodos para get, post, put y delete

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -1,20 +1,75 @@
 import { Injectable } from '@angular/core';
+import { Headers, Http } from '@angular/http';
 
-import { HEROES } from './mock-heroes';
 import { Hero } from './hero'
+
+import 'rxjs/add/operator/toPromise';
 
 @Injectable()
 export class HeroService {
-    getHeroes(){
-        return Promise.resolve(HEROES);
-    }
 
-    getHeroesSlowly() {
-        return new Promise<Hero[]>(resolve => setTimeout(() => resolve(HEROES), 2000));
+    private heroesUrl = 'app/heroes';   // URL to web api
+
+    constructor(private http: Http) { }
+
+    getHeroes(){
+        return this.http.get(this.heroesUrl)
+            .toPromise()
+            .then(response => response.json().data as Hero[])
+            .catch(this.handleError);
     }
 
     getHero(id: number) {
         return this.getHeroes()
             .then(heroes => heroes.find(hero => hero.id === id));
     }
+
+    // Si el heroe tiene id es un put, de lo contrario es un post
+    save(hero: Hero): Promise<Hero> {
+        if(hero.id) {
+            return this.put(hero);
+        }
+        return this.post(hero);
+    }
+
+    // Delete existing Hero
+    delete(hero: Hero) {
+        let headers = new Headers();
+        headers.append('Content-Type', 'application/json');
+        let url = `${this.heroesUrl}/${hero.id}`;
+        return this.http
+            .delete(url, {headers: headers})
+            .toPromise()
+            .catch(this.handleError);
+    }
+
+    // Add new Hero
+    private post(hero: Hero): Promise<Hero> {
+        let headers = new Headers({
+            'Content-Type': 'application/json'
+        });
+        return this.http
+            .post(this.heroesUrl, JSON.stringify(hero), {headers: headers})
+            .toPromise()
+            .then(res => res.json().data)
+            .catch(this.handleError);
+    }
+
+    // Update existing Hero
+    private put(hero: Hero) {
+        let headers = new Headers();
+        headers.append('Content-Type', 'application/json');
+        let url = `${this.heroesUrl}/${hero.id}` ;
+        return this.http
+            .put(url, JSON.stringify(hero), {headers: headers})
+            .toPromise()
+            .then(() => hero)
+            .catch(this.handleError);
+    }
+
+    private handleError(error: any) {
+        console.log('An error occurred', error);
+        return Promise.reject(error.message || error);
+        
+    } 
 }
